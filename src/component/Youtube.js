d39b7940d177d519f25881f550cc2ae99ac60d9f
ryantorecampo <ryan.torrecampo27@gmail.com>
2019-11-19 13:24:58 -0600

    Random video generation fixed

--- src/component/Youtube.js
+++ src/component/Youtube.js
@@ -1,83 +1,90 @@
-import React, { Component } from "react";
-import "./Youtube.scss";
+import React, { Component } from 'react';
+import './Youtube.scss';
 
-const API = "AIzaSyDSZGDjSA2wK7DlL7VoArJgM4HksYySoDM";
-const playlistId = "PLLlJxAcQth2uTyofPvsINUO9pO-hRWNTe";
+const API = 'AIzaSyDSZGDjSA2wK7DlL7VoArJgM4HksYySoDM';
+const playlistId = 'PLLlJxAcQth2uTyofPvsINUO9pO-hRWNTe';
 const result = 10;
 var finalURL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${result}&playlistId=${playlistId}&key=${API}`;
 
 class Youtube extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            resultyt: [],
-            index: 0,
-            nbrs: new Set()
-        };
-    }
-    componentDidMount() {
-        fetch(finalURL)
-            .then(response => response.json())
-            .then(responseJson => {
-                const resultyt = responseJson.items.map(
-                    obj =>
-                        "https://www.youtube.com/embed/" +
-                        obj.snippet.resourceId.videoId
-                );
-                this.setState({ resultyt });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			resultyt: [],
+			index: 0,
+			nbrs: new Set(),
+		};
+	}
+	componentDidMount() {
+		fetch(finalURL)
+			.then(response => response.json())
+			.then(responseJson => {
+				const resultyt = responseJson.items.map(
+					obj =>
+						'https://www.youtube.com/embed/' +
+						obj.snippet.resourceId.videoId
+				);
+				this.setState({ resultyt });
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
 
-    handleClick() {
-        this.setState(state => {
-            let res = Math.floor(Math.random() * result - 1);
-            let checker = true;
-            while (checker) {
-                if (this.state.nbrs.has(res)) {
-                    res = Math.floor(Math.random() * result - 1);
-                } else {
-                    this.state.nbrs.add(res);
-                    checker = false;
-                }
-            }
-            if (this.state.nbrs.size === result) {
-                this.state.nbrs.clear();
-            }
-            return { index: res };
-        });
-    }
+	handleClick() {
+		this.setState(state => {
+			// Once set is full, clear it
+			if (this.state.nbrs.size === this.state.resultyt.size) {
+				this.state.nbrs.size.clear();
+			}
 
-    render() {
-        return (
-            <div>
-                <div className="youtube">
-                    <iframe
-                        width="560"
-                        height="315"
-                        src={this.state.resultyt[this.state.index]}
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-                {this.frame}
-                <div id="container">
-                    <button
-                        class="learn-more"
-                        onClick={() => this.handleClick()}
-                    >
-                        <div class="circle">
-                            <span class="icon arrow"></span>
-                        </div>
-                        <p class="button-text">~Song</p>
-                    </button>
-                </div>
-            </div>
-        );
-    }
+			let res = Math.floor(Math.random() * result);
+			let currentSize = this.state.nbrs.size;
+			if (!this.state.nbrs.has(res)) {
+				this.state.nbrs.add(res);
+			} else {
+				do {
+					res = Math.floor(Math.random() * result);
+					this.state.nbrs.add(res);
+				} while (
+					this.state.nbrs.size === currentSize &&
+					this.state.nbrs.size < result
+				);
+			}
+
+			return { index: res };
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="youtube">
+					<iframe
+						width="560"
+						height="315"
+						src={this.state.resultyt[this.state.index]}
+						frameBorder="0"
+						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+					></iframe>
+				</div>
+				{this.frame}
+				<div id="container">
+					<h4 class="instruction">Press a button to generate.</h4>
+					<button
+						class="learn-more"
+						onClick={() => this.handleClick()}
+					>
+						<div class="circle">
+							<span class="icon arrow"></span>
+						</div>
+						<p class="button-text">Song</p>
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Youtube;
--- src/component/word.js
+++ src/component/word.js
@@ -1,39 +1,39 @@
-import React from "react";
-import "./word.scss";
+import React from 'react';
+import './word.scss';
 
 class Word extends React.Component {
-    render() {
-        var i = 0;
-        return (
-            <div className="wrapper">
-                <div id="container">
-                    <button
-                        class="learn-more"
-                        onClick={() => {
-                            this.props.handleClick();
-                        }}
-                    >
-                        <div class="circle">
-                            <span class="icon arrow"></span>
-                        </div>
-                        <p class="button-text">~Words</p>
-                    </button>
-                </div>
-                <div className="random-word">
-                    {this.props.word.map(item => {
-                        item.id = i;
-                        i++;
-                        // let timer = true;
-                        // function timeout_trigger() {
-                        //     timer = false;
-                        // }
-                        // setTimeout("timeout_trigger", 2000);
-                        return <div key={item.id}>{item.word}</div>;
-                    })}
-                </div>
-            </div>
-        );
-    }
+	render() {
+		var i = 0;
+		return (
+			<div className="wrapper">
+				<div id="container">
+					<button
+						class="learn-more"
+						onClick={() => {
+							this.props.handleClick();
+						}}
+					>
+						<div class="circle">
+							<span class="icon arrow"></span>
+						</div>
+						<p class="button-text">Words</p>
+					</button>
+				</div>
+				<div className="random-word">
+					{this.props.word.map(item => {
+						item.id = i;
+						i++;
+						// let timer = true;
+						// function timeout_trigger() {
+						//     timer = false;
+						// }
+						// setTimeout("timeout_trigger", 2000);
+						return <div key={item.id}>{item.word}</div>;
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Word;
